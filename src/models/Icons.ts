28c7a45e5bdc1fdf89fb0044164246c5817cd4b2
Victor Balbo <victorbalbo@gmail.com>
2024-03-09 11:24:56 -0300

    Add icons

---
+++ src/models/Icons.ts
@@ -0,0 +1,24 @@
+import PlaneIcon from '@/components/icons/plane-travel.svg'
+import BusIcon from '@/components/icons/bus.svg'
+import TrainIcon from '@/components/icons/train.svg'
+import BedIcon from '@/components/icons/bed.svg'
+
+export enum Icons {
+  Plane = 'Plane',
+  Bus = 'Bus',
+  Train = 'Train',
+  Bed = 'Bed',
+}
+
+export const getIconByName = (icon: string) => {
+  switch (icon) {
+    case Icons.Plane:
+      return PlaneIcon;
+    case Icons.Train:
+      return BusIcon;
+    case Icons.Bus:
+      return TrainIcon;
+    case Icons.Bed:
+      return BedIcon;
+  }
+}
